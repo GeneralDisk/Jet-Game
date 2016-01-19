e534d8c679118bf724b6a529bd68a6f114c901e3
Maris Kali <mkali@comcast.net>
2016-01-18 17:41:24 -0800

    Initial Commit

---
+++ Jet Game/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Jet Game
+//
+//  Created by Maris Kapualani Kali on 1/18/16.
+//  Copyright © 2016 Maris Kapualani Kali. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
